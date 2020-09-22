42d86a7a9697191338bf60b9cec6ae7e9ac16a93
ElRabie <rosaline.bakhit1@vodafone.com>
2020-09-22 16:50:15 +0200

    push

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 /** ChatBotService */
 @Injectable()
 export class AppService {
+    constructor(private http: HttpClient) {}
+    public getPromotions(): Observable<any> {
 
+        return this.http.get(' https://api.github.com/users').pipe(map((response) => {
+            console.log(response);
+            return response;
+        }));
+    }
 }
